310db79593d84c32bc0a7b4994d1c64a60731722
Jeyaram <jram.pet@gmail.com>
2021-05-11 10:41:06 +0530

    Initial Commit

---
+++ sections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sections
+//
+//  Created by jeyaram-pt4154 on 11/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
