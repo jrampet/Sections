7a32f630e7137060ad20a3e322ba44980741d426
Jeyaram <jram.pet@gmail.com>
2021-05-12 14:59:35 +0530

    force wrapping removed

--- sections/ViewController.swift
+++ sections/ViewController.swift
@@ -12,7 +12,7 @@ struct DataToShown {
     var color : UIColor
 }
 class CustomTapGestureRecognizer: UITapGestureRecognizer {
-    var tappedSection: Int?
+    var tappedSection: Int = 0
 }
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
@@ -26,7 +26,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     let datatoAdd = DataToShown(head: "Sign Out", body: "Your account will be logged out",color: UIColor.red)
     
     var sectionNames = ["Fixed","Auto Dimension","Calculated"]
-    var bodyofString = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -135,7 +135,7 @@ extension ViewController : UITableViewDataSource,UITableViewDelegate{
     
     @objc func handleTap(sender: CustomTapGestureRecognizer)
     {
-       let sec = sender.tappedSection!
+       let sec = sender.tappedSection
         let newIndexPath = IndexPath(row: descriptionData[sec].count, section: sec)
         descriptionData[sec].append(datatoAdd)
         tableView.insertRows(at: [newIndexPath], with: .fade)
